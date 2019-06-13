2740f3edf9cdf6ff5b47334386b716341252e497
Utkarsh Maheshwari <coditva@gmail.com>
2019-06-14 01:07:30 +0530

    Update deploy.sh to include changelog in commit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,16 +2,26 @@
 
 blog_repo="https://coditva:$GITHUB_TOKEN@github.com/coditva/blog"
 
-git clone $blog_repo blog/ || exit 1
-cd blog/
+latest_tag=$(git tag | tail -n1)
+previous_tag=$(git tag | tail -n2 | head -n1)
+changelog=$(git log --oneline --reverse $previous_tag..$latest_tag)
+
+if [[ -d "blog/" ]]; then
+    cd blog/
+    git pull
+else
+    git clone $blog_repo blog/ || exit 1
+    cd blog/
+fi
+
 cp ../resume.pdf ./static/resume.pdf || exit 1
 
 git config user.name  "Utkarsh Maheshwari"
 git config user.email "coditva@gmail.com"
 
-git commit -m "Update resume with TravisCI" static/resume.pdf || exit 0
+git commit -a -m "Update resume with TravisCI
 
-git remote rm origin
-git remote add origin $blog_repo
+changelog:
+$changelog"
 
 git push origin master
